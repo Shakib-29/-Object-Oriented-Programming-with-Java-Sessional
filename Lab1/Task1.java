b8664b5b9880f70b144dafc74639e1fd5e019f16
Touhidul Islam Shakib <touhidulislamshakib21@gmail.com>
2026-06-25 20:51:50 +0600

    Create Task1

---
+++ Lab1/Task1.java
@@ -0,0 +1,19 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        String name = "Touhidul Islam Shakib";
+        String studentID = "E253043";
+        int age = 21;
+        String phone = "01765523661";
+        String email = "touhidulislamshakib21@gmail.com";
+        String address = "Chattogram";
+
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+        System.out.println("Address: " + address);
+    }
+}
